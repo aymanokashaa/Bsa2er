3a4f9fe628e8e6cc849be0280e25d2f350344c7e
aymanokashaa <57822352+aymanokashaa@users.noreply.github.com>
2020-06-05 11:34:16 +0200

    commit stage

---
+++ Bsa2er MVC/Bsa2er MVC/Scripts/slide.js
@@ -0,0 +1,8 @@
+
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width = '330px';
+  }
+
+  function closeSlideMenu(){
+    document.getElementById('side-menu').style.width = '0';
+  }
