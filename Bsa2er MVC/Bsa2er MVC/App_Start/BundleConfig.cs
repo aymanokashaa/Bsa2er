8f9867e590feb4056c3fe3013719c42f5e27c26a
alcatrazor <a-icon73.5@hotmail.com>
2020-06-05 15:36:31 +0200

    Set Bootstrap

--- Bsa2er MVC/Bsa2er MVC/App_Start/BundleConfig.cs
+++ Bsa2er MVC/Bsa2er MVC/App_Start/BundleConfig.cs
@@ -8,35 +8,28 @@ namespace Bsa2er_MVC
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                     "~/Content/headerfooter.css",
+                     "~/Content/bootstrap-rtl.min.css",
+                     "~/Content/bootstrap.min.css",
                      "~/Content/font-awesome.css",
-                     "~/Content/bootstrap-rtl.min.css"));
+                     "~/Content/headerfooter.css"));
+
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
-                "~/Scripts/date.js",
+                "~/Scripts/slide.js",
                 "~/Scripts/date2.js",
+                "~/Scripts/date.js",
+                "~/Scripts/JQuery-3.5.1.min.js",
+                "~/Scripts/popper.js",
+                "~/Scripts/jquery.validate*",
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/jquery.nicescroll.min.js",
                 "~/Scripts/scroll.js",
-                "~/Scripts/show.js",
-                "~/Scripts/slide.js", 
-                "~/Scripts/show2.js",
-                 "~/Scripts/popper.js",
-            "~/Scripts/jquery.nicescroll.min.js"
+                "~/Scripts/show2.js"
                 ));
         }
     }
