2f65832b86d53344b5c50e80e20c84707c90d8c7
Ahmed Bauomy <65462259+Ahmed-Bauomy@users.noreply.github.com>
2020-07-05 16:22:43 +0200

    edit Admin DashBoard

--- Bsa2er MVC/Bsa2er MVC/Controllers/CertificationController.cs
+++ Bsa2er MVC/Bsa2er MVC/Controllers/CertificationController.cs
@@ -22,7 +22,7 @@ namespace Bsa2er_MVC.Controllers
         public ActionResult Create(int progId,string stdId)
         {
             var studentprogramInformation = db.StudentsPrograms.FirstOrDefault(s => s.Program_Id == progId && s.Std_Id == stdId);
-            ViewBag.studentName = studentprogramInformation.Student.ApplicationUser.UserName;
+            ViewBag.studentName = studentprogramInformation.Student.ApplicationUser.fullname;
             ViewBag.programName = studentprogramInformation.Program.Program_Title;
             ViewBag.grad = studentprogramInformation.ProgramGrade;
          
@@ -31,7 +31,7 @@ namespace Bsa2er_MVC.Controllers
         public ActionResult getCertification(int progId, string stdId)
         {
             var studentprogramInformation = db.StudentsPrograms.FirstOrDefault(s => s.Program_Id == progId && s.Std_Id == stdId);
-            ViewBag.studentName = studentprogramInformation.Student.ApplicationUser.UserName;
+            ViewBag.studentName = studentprogramInformation.Student.ApplicationUser.fullname;
             ViewBag.programName = studentprogramInformation.Program.Program_Title;
             ViewBag.grad = studentprogramInformation.ProgramGrade;
 
